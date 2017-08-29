80c552c51af74313a5dd1c2b95c90bd3707464cf
lprivette <lprivette@usgs.gov>
2017-08-29 12:53:14 -0400

    pushing so mitch and see

--- src/scripts/onload.js
+++ src/scripts/onload.js
@@ -1,5 +1,36 @@
 $( document ).ready(function() {
 
-  console.log("Page ready");
+  /* var eventList = $.parseJSON($.ajax({
+    url:  'https://stn.wim.usgs.gov/STNServices/Events.json',
+    dataType: "json",
+    async: false
+  }).responseText);
+  
+  var eventFiles = $.parseJSON($.ajax({
+    url:  'https://stn.wim.usgs.gov/STNServices/Events.json',
+    dataType: "json",
+    async: false
+  }).responseText);
 
+  var eventFiles = $.parseJSON($.ajax({
+    url:  'https://stn.wim.usgs.gov/STNServices/Events.json',
+    dataType: "json",
+    async: false
+  }).responseText); */
+  
+  $( "#test" ).on( "click", function() {
+    $.getJSON('https://stn.wim.usgs.gov/STNServices/Events.json', function(evt) {
+      var eventList = evt[0].event_name;
+  
+      /* $.each( evt, function( event_name, val ) {
+        items.push( "<li id='" + key + "'>" + val + "</li>" );
+      });
+      $( "<ul/>", {
+        "class": "grid-item",
+        html: items.join( "" )
+      }).appendTo( "#test" ); */
+    });
+  });
 });
+
+
